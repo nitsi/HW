b041a24a472ee9344ca56b1332001ea7c30fa757
Aviad Hahami <aviadhahami@gmail.com>
2015-04-24 22:32:47 +0300

    Let's go :+1:

--- Computer Graphics/CG_Ex3/proj/src/proj/ex3/Ex3.java
+++ Computer Graphics/CG_Ex3/proj/src/proj/ex3/Ex3.java
@@ -14,6 +14,7 @@ import proj.ex3.gui.MainFrame;
  * You basically don't need to add things here to the this package
  */
 public class Ex3 {
+	// DUMMY COMMIT
 
 	private static String getAboutMessage() {
 		// TODO: Add any specific instructions here if you changed any thing or
