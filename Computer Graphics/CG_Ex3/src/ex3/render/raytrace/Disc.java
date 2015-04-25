f6972f67f7ef16ed4d2f9449bbb0b179b7ccd707
Aviad Hahami <aviadhahami@gmail.com>
2015-04-25 19:18:31 +0300

    Done with Disc

--- Computer Graphics/CG_Ex3/src/ex3/render/raytrace/Disc.java
+++ Computer Graphics/CG_Ex3/src/ex3/render/raytrace/Disc.java
@@ -2,32 +2,59 @@ package ex3.render.raytrace;
 
 import java.util.Map;
 
-public class Disc extends Sphere{
+public class Disc extends Sphere {
 
-	private Vec normal;
-	
-	public Disc(){}
-	public Vec getNormal(){
-		return normal;
+	private static final String MISSING = "missing: ";
+	private static final String NO_NORMAL = "normal attr!";
+
+	private Vec g_normal;
+
+	// empty constructor
+	public Disc() {
 	}
-	
+
+	public Vec getNormal() {
+		return g_normal;
+	}
+
+	/**
+	 * 
+	 * @param attributes
+	 *            - from XML
+	 */
 	public Disc(Map<String, String> attributes) {
-	
 		super(attributes);
-		constractTheNormal(attributes);
+		generateNormal(attributes);
 	}
 
-
-	public void constractTheNormal(Map<String, String> attributes) {
-		if (!attributes.containsKey("normal")){
-			throw new IllegalArgumentException("missing normal");
-		}else{ 
-			normal = new Vec(attributes.get("normal"));
+	/**
+	 * generate vector from the normal attribute
+	 * 
+	 * @param attributes
+	 */
+	public void generateNormal(Map<String, String> attributes) {
+		if (verifyInput(attributes)) {
+			g_normal = new Vec(attributes.get("normal"));
 		}
+
 	}
+
+	/**
+	 * verifies XML input
+	 * 
+	 * @param attributes
+	 * @return true if all input tests passed
+	 */
+	private boolean verifyInput(Map<String, String> attributes) {
+		if (!attributes.containsKey("normal")) {
+			throw new IllegalArgumentException(MISSING + NO_NORMAL);
+		}
+		return true;
+	}
+
 	@Override
 	public Vec getNormalInPoint(Point3D p) {
-		return normal;
+		return g_normal;
 	}
 
 }
