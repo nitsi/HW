1bdf2f4e8bed6a2c5b7c574d91721a09d7e279b5
Aviad Hahami <aviadhahami@gmail.com>
2015-04-25 16:39:16 +0300

    dirLight && Light changed

--- Computer Graphics/CG_Ex3/proj/src/proj/ex3/render/raytrace/dirLight.java
+++ Computer Graphics/CG_Ex3/proj/src/proj/ex3/render/raytrace/dirLight.java
@@ -1,36 +1,43 @@
-/*
-Computer Graphics - Exercise 3
-Matan Gidnian	200846905
-Aviad Hahami	302188347
-*/
-
 package proj.ex3.render.raytrace;
+
 import java.util.Map;
 
 import proj.ex3.math.Point3D;
 import proj.ex3.math.Vec;
 
+
+
 public class dirLight extends Light {
-	
-	private Point3D direction = null;
-	
-	public dirLight()
-	{
-		this.direction = new Point3D("0 0 0");
-		super.color = new Vec("1 1 1");
-	}
-	
-	public void init(Map<String, String> attributes)
-	{
-		super.init(attributes);
-		if (attributes.containsKey("direction")) {
-			new Point3D(attributes.get("direction"));
-		}
+
+	private Vec direction;
+
+	public dirLight(){}
+	public dirLight(Map<String, String> attributes) {
+		init(attributes);
 	}
 
 	@Override
-	public Vec getLightAtPoint(Vec pnt)
-	{
+	public void init(Map<String, String> attributes)
+			throws IllegalArgumentException {
+
+		if (!attributes.containsKey("color")) {
+			color = new Vec(1, 1, 1);
+		} else {
+			color = new Vec(attributes.get("color"));
+		}
+		if (!attributes.containsKey("direction")) {
+			throw new IllegalArgumentException("missing direction to the light");
+		} else {
+			direction = new Vec(attributes.get("direction"));
+		}
+	}
+
+	public Vec getIntansityLight(Point3D p) {
 		return color;
 	}
+
+	public Vec getDirection() {
+		return direction;
+	}
+
 }
