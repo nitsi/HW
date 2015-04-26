5bfc23cbf9f8e2337b383ca89636428121c7ce98
Aviad Hahami <aviadhahami@gmail.com>
2015-04-26 05:14:03 +0300

    Done with sphere class
    :clap: :clap: :clap:

--- Computer Graphics/CG_Ex3/src/ex3/render/raytrace/Sphere.java
+++ Computer Graphics/CG_Ex3/src/ex3/render/raytrace/Sphere.java
@@ -8,48 +8,55 @@ package ex3.render.raytrace;
 import java.util.Map;
 
 public class Sphere extends Surface {
-
-	private double radius;
-	private Point3D center;
+	private static final String MISSING = "missing: ";
+	private static final String NO_RADIUS = "radius attr!";
+	private static final String NO_CENTER_POINT = "center point attr!";
+	private double g_sphereRadius;
+	private Point3D g_sphereCenter;
 
 	public Sphere() {
 	}
 
+	/**
+	 * Instantiate sphere using inputed attributes
+	 * 
+	 * @param attributes
+	 */
 	public Sphere(Map<String, String> attributes) {
 		originInit(attributes);
 		init(attributes);
 	}
 
 	@Override
-	public Vec getNormalAtPoint(Point3D p) {
-
-		Vec normal;
-		normal = Point3D.vectorBetweenTwoPoints(center, p);
-		normal.normalize();
-		return normal;
+	public Vec getNormalAtPoint(Point3D point) {
+		Vec i_normal;
+		i_normal = Point3D.vectorBetweenTwoPoints(g_sphereCenter, point);
+		i_normal.normalize();
+		return i_normal;
 	}
 
 	@Override
 	public void init(Map<String, String> attributes) throws IllegalArgumentException {
-
-		if (!attributes.containsKey("center")) {
-			throw new IllegalArgumentException("missing center point");
+		// test and set input
+		if (attributes.containsKey("center")) {
+			g_sphereCenter = new Point3D(attributes.get("center"));
 		} else {
-			center = new Point3D(attributes.get("center"));
+			throw new IllegalArgumentException(MISSING + NO_CENTER_POINT);
 		}
-		if (!attributes.containsKey("radius")) {
-			throw new IllegalArgumentException("missing radius");
+
+		if (attributes.containsKey("radius")) {
+			g_sphereRadius = Double.parseDouble(attributes.get("radius"));
 		} else {
-			radius = Double.parseDouble(attributes.get("radius"));
+			throw new IllegalArgumentException(MISSING + NO_RADIUS);
 		}
 
 	}
 
 	public Point3D getCenter() {
-		return center;
+		return g_sphereCenter;
 	}
 
 	public double getRadius() {
-		return radius;
+		return g_sphereRadius;
 	}
 }
