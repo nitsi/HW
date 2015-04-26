b2a94e635a9e41c5de3827edd28844916eea015c
Aviad Hahami <aviadhahami@gmail.com>
2015-04-26 10:20:53 +0300

    Done with spotlight

--- Computer Graphics/CG_Ex3/src/ex3/render/raytrace/SpotLight.java
+++ Computer Graphics/CG_Ex3/src/ex3/render/raytrace/SpotLight.java
@@ -9,70 +9,73 @@ import java.util.Map;
 
 public class SpotLight extends Light {
 
-	private Vec direction;
-	private Point3D position;
-	private double kConst;
-	private double kLinear;
-	private double kQuadratic;
+	private static final String MISSING = "missing: ";
+	private static final String NO_POS = "pos attr!";
+	private static final String NO_DIRECTION_TO_LIGHT = "dir attr!";
+
+	private Vec g_directionVector;
+	private Point3D g_positionPoint;
+	private double g_kConst;
+	private double g_kLinear;
+	private double g_kQuadratic;
 
 	public SpotLight() {
 	}
 
 	public Point3D getPosition() {
-		return position;
+		return this.g_positionPoint;
 	}
 
+	/**
+	 * Instantiates SpotLight using Light attributes constructor
+	 * 
+	 * @param attributes
+	 */
 	public SpotLight(Map<String, String> attributes) {
 		init(attributes);
 	}
 
+	// Guess what, I lied. We override the constructor
 	@Override
 	public void init(Map<String, String> attributes) throws IllegalArgumentException {
-
-		if (!attributes.containsKey("color")) {
-			g_color = new Vec(1, 1, 1);
-		} else {
-			g_color = new Vec(attributes.get("color"));
-		}
+		// killer attributes
 		if (!attributes.containsKey("pos")) {
-			throw new IllegalArgumentException("missing position to the light");
-		} else {
-			position = new Point3D(attributes.get("pos"));
-		}
-		if (attributes.containsKey("kc")) {
-			kConst = Double.parseDouble(attributes.get("kc"));
-		} else {
-			kConst = 1;
-		}
-
-		if (attributes.containsKey("kl")) {
-			kLinear = Double.parseDouble(attributes.get("kl"));
-		} else {
-			kLinear = 0;
-		}
-
-		if (attributes.containsKey("kq")) {
-			kQuadratic = Double.parseDouble(attributes.get("kq"));
-		} else {
-			kQuadratic = 0;
+			throw new IllegalArgumentException(MISSING + NO_POS);
 		}
 		if (!attributes.containsKey("dir")) {
-			throw new IllegalArgumentException("missing direction to the light");
-		} else {
-			direction = new Vec(attributes.get("dir"));
-			direction.normalize();
+			throw new IllegalArgumentException(MISSING + NO_DIRECTION_TO_LIGHT);
 		}
+
+		// if we got here, we populate variables with defaults / data from XML
+		g_positionPoint = new Point3D(attributes.get("pos"));
+
+		g_color = attributes.containsKey("color") ? new Vec(attributes.get("color")) : new Vec("1 1 1");
+
+		g_kConst = attributes.containsKey("kc") ? Double.parseDouble(attributes.get("kc")) : 1;
+
+		g_kLinear = attributes.containsKey("kl") ? Double.parseDouble(attributes.get("kl")) : 0;
+		g_kQuadratic = attributes.containsKey("kq") ? Double.parseDouble(attributes.get("kq")) : 0;
+
+		g_directionVector = new Vec(attributes.get("dir"));
+		g_directionVector.normalize();
 
 	}
 
-	public Vec getLightIntensity(Point3D p) {
+	/**
+	 * returns the light intensity in a given point
+	 */
+	public Vec getLightIntensity(Point3D point) {
 
-		Vec locationRelativeToLightSource = Point3D.vectorBetweenTwoPoints(p, position);
-		locationRelativeToLightSource.normalize();
-		double angleBetweenObjectAndLight = Math.max(Vec.dotProd(locationRelativeToLightSource, direction), 0);
-		double lengthFromHit = p.distance(position);
-		Vec IL = Vec.scale(1 / (kConst + kLinear * lengthFromHit + kQuadratic * lengthFromHit * lengthFromHit),
-				Vec.scale(angleBetweenObjectAndLight, g_color));
-		return IL;
+		Vec i_locationWithRespectToLight = Point3D.vectorBetweenTwoPoints(point, g_positionPoint);
+		i_locationWithRespectToLight.normalize();
+		// calculate the angle
+		double i_tempDotProd = Vec.dotProd(i_locationWithRespectToLight, g_directionVector);
+		double i_angelBetweenLightAndDirectionVector = i_tempDotProd < 0 ? 0 : i_tempDotProd;
+
+		double i_distanceFromPoint = point.distance(g_positionPoint);
+		// calculate and return IL according to formula
+		return Vec.scale(1 / (g_kConst + g_kLinear * i_distanceFromPoint + g_kQuadratic * i_distanceFromPoint * i_distanceFromPoint),
+				Vec.scale(i_angelBetweenLightAndDirectionVector, g_color));
+
 	}
 }
