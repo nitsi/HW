b1ff6b1f917823bca8cd6b0e0a6835c9f69da2d7
Aviad Hahami <aviadhahami@gmail.com>
2015-04-26 10:38:08 +0300

    Done with surface :surfer:

--- Computer Graphics/CG_Ex3/src/ex3/render/raytrace/Surface.java
+++ Computer Graphics/CG_Ex3/src/ex3/render/raytrace/Surface.java
@@ -2,68 +2,55 @@ package ex3.render.raytrace;
 
 import java.util.Map;
 
-public abstract class Surface implements IInitable{
+public abstract class Surface implements IInitable {
 
-	protected Vec mltDifuse;
-	protected Vec mltSpecular;
-	protected Vec mltAmbient;
-	protected Vec mltEmission;
-	protected double mltShininess;
-	protected double reflectance;
-	
-	public  Double getReflectance(){
-		return reflectance;
+	protected Vec g_materialDifuse;
+	protected Vec g_materialSpecular;
+	protected Vec g_materialAmbient;
+	protected Vec g_materialEmission;
+	protected double g_materialShininess;
+	protected double g_materialReflectance;
+
+	public Double getReflectance() {
+		return g_materialReflectance;
 	}
-	public Double getShininess(){
-		return mltShininess;
+
+	public Double getShininess() {
+		return g_materialShininess;
 	}
-	public Vec getEmission(){
-		return mltEmission;
+
+	public Vec getEmission() {
+		return g_materialEmission;
 	}
-	public Vec getDifuse(){
-		return mltDifuse;
+
+	public Vec getDifuse() {
+		return g_materialDifuse;
 	}
-	
-	protected void originInit(Map<String, String> attributes){
+
+	protected void originInit(Map<String, String> attributes) {
 		
-		if (!attributes.containsKey("mtl-diffuse")){
-			mltDifuse = new Vec(0.7, 0.7, 0.7);
-		}else{ 
-			mltDifuse = new Vec(attributes.get("mtl-diffuse"));
-		}
-		if (!attributes.containsKey("mtl-specular")){
-			mltSpecular = new Vec(1, 1, 1);
-		}else{ 
-			mltSpecular = new Vec(attributes.get("mtl-specular"));
-		}
-		if (!attributes.containsKey("ambient-light")){
-			mltAmbient = new Vec(0.1, 0.1, 0.1);
-		}else{ 
-			mltAmbient = new Vec(attributes.get("mtl-ambient"));
-		}
-		if (!attributes.containsKey("mtl-emission")){
-			mltEmission = new Vec(0, 0, 0);
-		}else{ 
-			mltEmission = new Vec(attributes.get("mtl-emission"));
-		}
-		if (!attributes.containsKey("mtl-shininess")){
-			mltShininess = 100;
-		}else{ 
-			mltShininess = Double.parseDouble(attributes.get("mtl-shininess"));
-		}
-		if (!attributes.containsKey("reflectance")){
-			reflectance = 0;
-		}else{ 
-			reflectance = Double.parseDouble(attributes.get("reflectance"));
-		}
+		// populate variables according to attributes checks, if attributes doesn't exist -> we use fallback value
+		g_materialDifuse = attributes.containsKey("mtl-diffuse") ? new Vec(attributes.get("mtl-diffuse")) : new Vec(0.7, 0.7, 0.7);
+		g_materialSpecular = attributes.containsKey("mtl-specular") ? new Vec(attributes.get("mtl-specular")) : new Vec(1, 1, 1);
+		g_materialAmbient = attributes.containsKey("ambient-light") ? new Vec(attributes.get("mtl-ambient")) : new Vec(0.1, 0.1, 0.1);
+		g_materialEmission = attributes.containsKey("mtl-emission") ? new Vec(attributes.get("mtl-emission")) : new Vec("0 0 0");
+		g_materialShininess = attributes.containsKey("mtl-shininess") ? Double.parseDouble(attributes.get("mtl-shininess")) : 100;
+		g_materialReflectance = attributes.containsKey("reflectance") ? Double.parseDouble(attributes.get("reflectance")) : 0;
 	}
-	public abstract Vec getNormalAtPoint(Point3D p);
+
+	/**
+	 * 
+	 * @param point
+	 * @return g_ambient and g_specular
+	 */
+	public abstract Vec getNormalAtPoint(Point3D point);
 
 	public Vec getAmbiant() {
-		return mltAmbient;
+		return g_materialAmbient;
 	}
+
 	public Vec getSpecular() {
-		return mltSpecular;
+		return g_materialSpecular;
 	}
-	
+
 }
