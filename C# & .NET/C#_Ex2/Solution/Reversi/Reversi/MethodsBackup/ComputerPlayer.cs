d80e4f71df7474a6e0c7f832ddf66e81ab9ea290
Aviad Hahami <aviadhahami@gmail.com>
2015-05-18 00:02:38 +0300

    Added Backup

---
+++ C# & .NET/C#_Ex2/Solution/Reversi/Reversi/MethodsBackup/ComputerPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reversi
+{
+    class ComputerPlayer
+    {
+        private int m_AmountOfPoints;
+
+        public ComputerPlayer(int p)
+        {
+            // TODO: Complete member initialization
+            this.m_AmountOfPoints = p;
+        }
+    }
+}
---
+++ C# & .NET/C#_Ex2/Solution/Reversi/Reversi/MethodsBackup/GameBoard.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text;
+
+namespace Reversi
+{
+    enum Pions
+    {
+        BLACK, // marks black
+        WHITE, // marks white
+        EMPTY
+    }
+
+    class GameBoard
+    {
+        private int m_BoardSize;
+        private Pions[,] m_Board;
+
+        private string m_Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public GameBoard(int i_BoardSize)
+        {
+            m_BoardSize = i_BoardSize;
+            m_Board = new Pions[m_BoardSize, m_BoardSize];
+            initBoard();
+        }
+
+        private void initBoard()
+        {
+            // init the array with empty
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                for (int j = 0; j < m_BoardSize; j++)
+                {
+                    m_Board[i, j] = Pions.EMPTY;
+                }
+            }
+
+            int i_BoardMiddle = m_BoardSize / 2;
+
+            // Append BLACK
+            m_Board[i_BoardMiddle, i_BoardMiddle] = Pions.BLACK;
+            m_Board[i_BoardMiddle - 1, i_BoardMiddle - 1] = Pions.BLACK;
+
+            // Append WHITE
+            m_Board[i_BoardMiddle - 1, i_BoardMiddle] = Pions.WHITE;
+            m_Board[i_BoardMiddle, i_BoardMiddle - 1] = Pions.WHITE;
+        }
+
+        internal bool GotMoreValidMoves()
+        {
+            return true;
+        }
+
+        internal void PrintCurrentState()
+        {
+            generateTopLetters();
+            generateNewLine();
+            generateLineSeparators();
+            generateNewLine();
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                generateLineNumber(i);
+                for (int j = 0; j < m_BoardSize; j++)
+                {
+                    generateColumnSeparator();
+                    generateCellContent(i, j);
+                }
+
+                generateNewLine();
+                generateLineSeparators();
+                generateNewLine();
+            }
+        }
+
+        private void generateCellContent(int i, int j)
+        {
+            Pions i_TempPionContent = m_Board[i, j];
+            if (i_TempPionContent == Pions.EMPTY)
+            {
+                Console.Write(" ");
+            }
+            else if (i_TempPionContent == Pions.BLACK)
+            {
+                Console.Write("X");
+            }
+            else
+            {
+                Console.Write("O");
+            }
+        }
+
+        private void generateColumnSeparator()
+        {
+            Console.Write(" | ");
+        }
+
+        private void generateLineNumber(int i)
+        {
+            Console.Write((i + 1) + " )");
+        }
+
+        private void generateLineSeparators()
+        {
+            generateBorderSpan();
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                Console.Write("====");
+            }
+        }
+
+        private void generateBorderSpan()
+        {
+            Console.Write("    ");
+        }
+
+        private void generateTopLetters()
+        {
+            // Generate top letters
+            generateBorderSpan();
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                Console.Write("  " + m_Alphabet[i] + " ");
+            }
+        }
+
+        private void generateNewLine()
+        {
+            Console.WriteLine();
+        }
+
+        internal bool CheckIfValid(string i_PlayerMove)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AppendMove(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string CalculateWinner()
+        {
+
+            int i_WhitePoints = 0;
+            int i_BlackPoints = 0;
+
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                for (int j = 0; j < m_BoardSize; j++)
+                {
+                    if (m_Board[i, j] == Pions.WHITE)
+                    {
+                        i_WhitePoints++;
+                    }
+                    else if (m_Board[i, j] == Pions.BLACK)
+                    {
+                        i_BlackPoints++;
+                    }
+                }
+            }
+            return i_BlackPoints > i_WhitePoints ? Pions.BLACK.ToString() : Pions.WHITE.ToString();
+        }
+    }
+}
