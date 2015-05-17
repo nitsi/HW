a5d0d34de0e6b41af71a4fa9e24caa24af8af532
Aviad Hahami <aviadhahami@gmail.com>
2015-05-17 18:48:27 +0300

    Started to work

---
+++ C# & .NET/C#_Ex2/Solution/Reversi/Reversi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReversiGame i_MainGame = new ReversiGame();
+            i_MainGame.start();
+        }
+    }
+}
