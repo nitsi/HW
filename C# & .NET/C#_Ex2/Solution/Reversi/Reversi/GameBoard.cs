2ce4c61eb017bdff95548a39bf6519eb09a5951d
Aviad Hahami <aviadhahami@gmail.com>
2015-05-18 21:10:44 +0300

    Starting new logic for propagation

--- C# & .NET/C#_Ex2/Solution/Reversi/Reversi/GameBoard.cs
+++ C# & .NET/C#_Ex2/Solution/Reversi/Reversi/GameBoard.cs
@@ -364,13 +364,22 @@ namespace Reversi
         private void updateFromLocation(int i_X, int i_Y, Colors i_GivenColor)
         {
             Console.WriteLine("updating stuff");
+            List<int[]> io_propagationCandidates = new List<int[]>();
+
             // Update vertical
             propagatePositiveVertical(i_X, i_Y, i_GivenColor);
             propagateNegativeVertical(i_X, i_Y, i_GivenColor);
             // Update Horizontal
             propagatePositiveHorizontal(i_X, i_Y, i_GivenColor);
             propagatePositiveHorizontal(i_X, i_Y, i_GivenColor);
+            // Update slash
+            propagatePositiveSlash(i_X, i_Y, i_GivenColor);
+            propagateNegativeSlash(i_X, i_Y, i_GivenColor);
+            // Update backslash
+            propagatePositiveBackslash(i_X, i_Y, i_GivenColor);
+            propagateNegativeBackslash(i_X, i_Y, i_GivenColor);
         }
+
 
         internal Colors CalculateWinner()
         {
